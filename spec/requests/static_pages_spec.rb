0f38b37dfcb3d6e302a867b817f72c265fdb5c3c
Merus <mattcrampy@gmail.com>
2012-07-11 02:33:03 +1000

    added contact, migration to postgresql

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) {"Blather"}
+
 	describe "Home page" do
 
 		it "should have the h1 'Share Your Interests (and identity)'" do
@@ -12,7 +14,7 @@ describe "Static pages" do
 		it "should have the title 'Home'" do
 	  		visit '/static_pages/home'
 	  		page.should have_selector('title',
-						:text => "Blather | Home")
+						:text => "#{base_title} | Home")
 		end
   	end
 
@@ -26,7 +28,7 @@ describe "Static pages" do
 		it "should have the title 'Help'" do
 	  		visit '/static_pages/help'
 			page.should have_selector('title',
-						:text => "Blather | Help")
+						:text => "#{base_title} | Help")
 		end
   	end
 
@@ -40,7 +42,22 @@ describe "Static pages" do
 		it "should have the title 'About Us'" do
 	  		visit '/static_pages/about'
 	  		page.should have_selector('title',
-					:text => "Blather | About Us")
+					:text => "#{base_title} | About Us")
 		end
   	end
+
+	describe "About page" do
+
+		it "should have the h1 'Contact Us'" do
+	  		visit '/static_pages/contact'
+	  		page.should have_selector('h1', :text => 'Contact Us')
+		end
+
+		it "should have the title 'Contact Us'" do
+	  		visit '/static_pages/contact'
+	  		page.should have_selector('title',
+					:text => "#{base_title} | Contact Us")
+		end
+  	end
+
 end
